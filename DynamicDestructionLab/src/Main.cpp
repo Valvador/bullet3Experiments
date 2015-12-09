ab9d57b459cb73b3b6272b494d064527866f669b
iheartdata <tracy.lee.lyon@gmail.com>
2015-12-08 23:23:45 -0800

    Test Program for testing Triangle Division

--- DynamicDestructionLab/src/Main.cpp
+++ DynamicDestructionLab/src/Main.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+	Triangle testTriangle;
+	testTriangle.push_back(btVector3(-1.0, 0, -1.0));
+	testTriangle.push_back(btVector3(1.0, 0, -1.0));
+	testTriangle.push_back(btVector3(0.0, 0, 1.0));
 
+	btVector3 plane(-1.0, 0, 0);
+	btVector3 pointOnPlane(-0.1, 0, 0);
+
+	Triangles leftTriangles;
+	Triangles rightTriangles;
+	MeshTools::ClipTriangle(testTriangle, plane, pointOnPlane, leftTriangles, rightTriangles);
+
+	printf("Left Triangle Count: %d \n", leftTriangles.size());
+	printf("Right Triangle Count: %d \n", rightTriangles.size());
 }
